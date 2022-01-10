e29ed118b72abb0621ec7210e550b039df558a24
neil-hughes <neil.hughes@techreturners.com>
2022-01-10 15:51:49 +0000

    Updated specimen complete code for TypeScript

--- src/components/appointment.test.complete.tsx
+++ src/components/appointment.test.complete.tsx
@@ -1,12 +1,14 @@
+import Appointment, { AppointmentProps } from './appointment';
+
 import { render, screen } from '@testing-library/react';
-import Appointment from './appointment';
 import userEvent from '@testing-library/user-event';
 
 describe('Appointment component', () => {
 	it(`Given the required props,
-        When the component is rendered,
-        Then the appointment data should be present`, () => {
-		const requiredProps = {
+		When the component is rendered,
+		Then the appointment data should be present`, () => {
+		const requiredProps: AppointmentProps = {
+			// any props that are required, i.e. not marked as optional with '?'
 			id: 1,
 			person: 'Harriet',
 			description: 'A very special appointment',
@@ -14,21 +16,22 @@ describe('Appointment component', () => {
 		};
 
 		render(<Appointment {...requiredProps} />);
+
 		expect(screen.getByText(requiredProps.description)).toBeInTheDocument();
-		expect(screen.getByText(requiredProps.id)).toBeInTheDocument();
-		expect(screen.getByText(requiredProps.person)).toBeInTheDocument();
 		expect(screen.getByText(requiredProps.date)).toBeInTheDocument();
+		expect(screen.getByText(requiredProps.person)).toBeInTheDocument();
 	});
 
 	it(`Given an unconfirmed appointment,
-            When the component is rendered,
-            Then the confirm button should be present`, () => {
-		const unconfirmedAppointment = {
+		When the component is rendered,
+		Then the confirm button should be present`, () => {
+		const unconfirmedAppointment: AppointmentProps = {
 			id: 1,
 			person: 'Harriet',
 			description: 'A very special appointment',
 			date: '3 Nov',
 			confirmed: false,
+			onMarkConfirmed: () => {},
 		};
 
 		render(<Appointment {...unconfirmedAppointment} />);
@@ -36,20 +39,20 @@ describe('Appointment component', () => {
 		expect(
 			screen
 				.getAllByRole('button')
-				.filter((button) => button.textContent === 'Mark confirmed')
-				.length
-		).toBe(1);
+				.find((button) => button.textContent === 'Mark confirmed')
+		).toBeInTheDocument();
 	});
 
-	it(`Given a confirmed appointment,
-            When the component is rendered,
-            Then the confirm button should not be present`, () => {
-		const confirmedAppointment = {
+	it(`Given an confirmed appointment,
+		When the component is rendered,
+		Then the confirm button should not be present`, () => {
+		const confirmedAppointment: AppointmentProps = {
 			id: 1,
 			person: 'Harriet',
 			description: 'A very special appointment',
 			date: '3 Nov',
 			confirmed: true,
+			onMarkConfirmed: () => {},
 		};
 
 		render(<Appointment {...confirmedAppointment} />);
@@ -57,14 +60,13 @@ describe('Appointment component', () => {
 		expect(
 			screen
 				.queryAllByRole('button')
-				.filter((button) => button.textContent === 'Mark confirmed')
-				.length
-		).toBe(0);
+				.find((button) => button.textContent === 'Mark confirmed')
+		).toBeUndefined();
 	});
 
 	it(`Given an appointment is rendered,
-                When the confirm button is clicked,
-                Then the onMarkConfirmed function should be called`, () => {
+		When the done button is clicked,
+		Then the onMarkConfirmed function is called with the correct appointmentId`, () => {
 		const mockConfirm = jest.fn();
 		const unconfirmedAppointment = {
 			id: 1,
@@ -83,13 +85,15 @@ describe('Appointment component', () => {
 
 		expect(confirmButton).toBeInTheDocument();
 
-		userEvent.click(confirmButton);
+		if (confirmButton) {
+			userEvent.click(confirmButton);
+		}
 
-		userEvent.click(confirmButton);
+		expect(mockConfirm.mock.calls.length).toBe(1);
 
-		expect(mockConfirm.mock.calls.length).toBe(2);
-
-		// check it's been called with the right parameters
 		expect(mockConfirm.mock.calls[0][0]).toBe(unconfirmedAppointment.id);
+
+		expect(mockConfirm).toHaveBeenCalledTimes(1);
+		expect(mockConfirm).toHaveBeenCalledWith(unconfirmedAppointment.id);
 	});
 });
